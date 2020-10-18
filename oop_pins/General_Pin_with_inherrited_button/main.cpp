762c901ccb0ba94b7e05b8722dc0ea74d3147549
kajnorman <knn@eaaa.dk>
2020-10-18 09:30:11 +0200

    Update main.cpp
    main prg. uses all functionality

--- oop_pins/General_Pin_with_inherrited_button/main.cpp
+++ oop_pins/General_Pin_with_inherrited_button/main.cpp
@@ -4,32 +4,42 @@
  Version  0.7
  
 The purpose with this task is to make a library that does 
-		some of the Arduino-like handling of io-pins
+		some of the Arduino-like handling of io-pins(but OO) 
+			digitalRead()
+			digitalWrite()
+			PORTPin()  //constructor
+		a button derived from the portpin
+			Pressed_since_last();
+			Wait_for_Press();			
 
 Example :
-	#include  "iolib.h"
+
+		Button startbutton = Button(D,7,0);  //sets PORTD.7 to be an input (last argument 1 for out 0 for in)
+		PORTPin statusled = PORTPin(D,6,1);  //sets PORTD.6 to be an output (last argument 1 for out 0 for in)
+		startbutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
 		
-	int main(void)
-	{
-	
-		PORTPin button = PORTPin(B,0,0);  //sets PORTB.0 to be an input (last argument 1 for out 0 for in)
+		Button togglebutton = Button(B,0,0);  //sets PORTB.0 to be an input (last argument 1 for out 0 for in)
 		PORTPin redled = PORTPin(B,1,1);  //sets PORTB.1 to be an output (last argument 1 for out 0 for in)
-		button.digitalWrite(1); //writing 1 to an input allows setting the pullup
+		togglebutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
+
+		startbutton.Wait_for_Press();  //blocking untill pressed
+		statusled.digitalWrite(true);
 
 		while(1)
 		{
-			bool flag;  //The LED will light while the button is being pressed
-			flag = button.digitalRead();
-			redled.digitalWrite(flag);
-			// or redled.digitaWrite(button.digitalRead());
+			if (togglebutton.Pressed_since_last())
+			{
+				redled.digitalWrite(! redled.digitalRead());
+			}
+			_delay_ms(10);
 		}
-	}
 
  * Author : knn
  */ 
 
 #include <avr/io.h>
 #include <stdbool.h>
+#define F_CPU 8000000
 #include "util/delay.h"
 
 
@@ -122,9 +132,23 @@ public:
 Button(PortnameType p, char pinr,bool direction) :  PORTPin(p,pinr,direction){}
 //The button constructor only runs its "parent"
 
-bool Pressed_since_last(); 
+bool Pressed_since_last();
+void Wait_for_Press();
 
 };
+
+
+void Button::Wait_for_Press()
+{   //  Blocking function call  !!
+	while (digitalRead() == true)
+		{
+			//Do nothing
+		}
+	
+}
+
+
+
 
 bool Button::Pressed_since_last()
 //returns true if the state of the button has changed from
@@ -151,18 +175,28 @@ return temp_ret;
 
 int main(void)
 {
-		
-	Button button = Button(B,0,0);  //sets PORTB.0 to be an input (last argument 1 for out 0 for in)
+	Button startbutton = Button(D,7,0);  //sets PORTD.7 to be an input (last argument 1 for out 0 for in)
+	PORTPin statusled = PORTPin(D,6,1);  //sets PORTD.6 to be an output (last argument 1 for out 0 for in)
+	startbutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
+				
+	Button togglebutton = Button(B,0,0);  //sets PORTB.0 to be an input (last argument 1 for out 0 for in)
 	PORTPin redled = PORTPin(B,1,1);  //sets PORTB.1 to be an output (last argument 1 for out 0 for in)
-	button.digitalWrite(1); //writing 1 to an input allows setting the pullup
+	togglebutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
+
+	startbutton.Wait_for_Press();  //blocking untill pressed
+	statusled.digitalWrite(true);
 
 	while(1)
 	{
-		if (button.Pressed_since_last())
+		if (togglebutton.Pressed_since_last())
 		{
 			redled.digitalWrite(! redled.digitalRead());
 		}
 		_delay_ms(10);
 	}
 }
+
+
+
+
 
