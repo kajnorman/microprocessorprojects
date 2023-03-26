6b873b9f65f40f42e94cd44af77f5c12f70c49c9
kajnorman <knn@eaaa.dk>
2023-03-26 18:58:13 +0200

    Update main.c
    known error/issue with using ins and outs on the same port.
    This is because ins in this case is for pulled up buttons.
    The pullup is disturbed by this software library.
    when a bit is written to the outputs all bits other bits are "refreshed" to their state, this means that pullup will be reset if a button is pressed.
    set and clr should be used

--- oop_pins/pin_and_inherited_button/main.c
+++ oop_pins/pin_and_inherited_button/main.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-	Button startbutton = Button(D,5,0);  //sets PORTD.7 to be an input (last argument 1 for out 0 for in)
+	Button startbutton = Button(B,5,0);  //sets PORTB.5 to be an input (last argument 1 for out 0 for in)
 	PORTPin statusled = PORTPin(D,6,1);  //sets PORTD.6 to be an output (last argument 1 for out 0 for in)
 	startbutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
 	
 	Button togglebutton = Button(B,0,0);  //sets PORTB.0 to be an input (last argument 1 for out 0 for in)
-	PORTPin redled = PORTPin(B,1,1);  //sets PORTB.1 to be an output (last argument 1 for out 0 for in)
+	PORTPin redled = PORTPin(D,1,1);  //sets PORTD.1 to be an output (last argument 1 for out 0 for in)
 	togglebutton.digitalWrite(1); //writing 1 to an input allows setting the pullup
 
 	startbutton.Wait_for_Press();  //blocking untill pressed
