320c74c50e93be3145348a73eace13b449ec69ba
kajnorman <knn@eaaa.dk>
2023-12-04 11:12:32 +0100

    Update list_files.py

--- picomicropython/tricks/list_files.py
+++ picomicropython/tricks/list_files.py
@@ -1,1 +1,10 @@
-
+'''
+import os
+#Then try listing the contents of the filesystem:
+os.listdir()
+#You can make directories:
+os.mkdir('dir')
+#And remove entries:
+os.remove('data.txt')
+'''
+print("OK")
